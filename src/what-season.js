1635202bb08fa19679314221d36ea5bb10b426ac
DoroPomidoro <pomidorodoro@gmail.com>
2022-04-29 02:18:32 +0300

    fix: resolve what season

--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if(!date) {
+    return 'Unable to determine the time of year!'
+  } else {
+    if ((Object.prototype.toString.call(date) === '[object Date]') || (Object.getOwnPropertySymbols(date).length > 0 )) {
+      let month = date.getMonth();
+      if (month === 0 || month === 1 || month === 11) {
+        return "winter"
+      } else if (month === 2 || month === 3 || month === 4) {
+        return "spring" 
+      } else if (month === 5 || month === 6 || month === 7) {
+        return "summer"
+      } else if (month === 8 || month === 9 || month === 10) {
+        return "autumn"
+      } else {      
+      return 'Unable to determine the time of year!';
+      }
+    } else {
+      throw new Error("Invalid date!");
+    }
+  }
 }
+
+
+// function getSeason(date) {
+//   if(!date) {
+//     return 'Unable to determine the time of year!'
+//   } else {
+//     if (Object.prototype.toString.call(date) !== '[object Date]' || Object.getOwnPropertySymbols(date).length !== 0)  throw new Error("Invalid date!") 
+      
+    
+//       let month = date.getMonth();
+//       if (month === 0 || month === 1 || month === 11) {
+//         return "winter"
+//       } else if (month === 2 || month === 3 || month === 4) {
+//         return "spring" 
+//       } else if (month === 5 || month === 6 || month === 7) {
+//         return "summer"
+//       } else if (month === 8 || month === 9 || month === 10) {
+//         return "autumn"
+//       } else {      
+//       return 'Unable to determine the time of year!';
+//       }
+//     } 
+//   }
+
 
 module.exports = {
   getSeason
