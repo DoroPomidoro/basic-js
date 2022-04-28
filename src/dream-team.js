6f9cf211f39366eabdfc1794d33f5a1b4302f606
DoroPomidoro <pomidorodoro@gmail.com>
2022-04-28 16:24:50 +0300

    feat: solve dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// function createDreamTeam(members) {
+//   let finalArray = [];
+//   let firstLetter = [];
+//   let arrWithoutWhitespaces = []
+//   let sortedMembers;
+//   if(typeof members !== "object" && ) {
+//     return false; 
+//   } else {
+//   for (let i = 0; i < members.length; i++) {
+//     if (typeof members[i] !== "string") {
+//       return false;
+//     } else {
+//      arrWithoutWhitespaces = members[i].split('');
+//      for (let j = 0; j < arrWithoutWhitespaces.length; j++) {
+//        if (arrWithoutWhitespaces[j] === ' ') {
+//          continue
+//        } else {
+//          firstLetter.push(arrWithoutWhitespaces[j])
+//        }
+//      }
+//      sortedMembers = firstLetter.join('').charAt(0).toUpperCase();
+//      finalArray.push(sortedMembers);
+//      firstLetter = [];
+//     }
+//   }
+  
+//   return finalArray.sort().join('');
+// }
+// }
+
+function createDreamTeam(members) {
+  let finalArray = [];
+  let firstLetter = [];
+  let arrWithoutWhitespaces = []
+  let sortedMembers;
+    if (members==null){
+      return false;
+     } else {
+    for (let i = 0; i < members.length; i++) {
+      if ((typeof members[i] !== "string" || typeof members[i] === "undefined")) {
+        delete members[i]
+      } else {
+       arrWithoutWhitespaces = members[i].split('');
+       for (let j = 0; j < arrWithoutWhitespaces.length; j++) {
+         if (arrWithoutWhitespaces[j] === ' ') {
+           continue
+         } else {
+           firstLetter.push(arrWithoutWhitespaces[j])
+         }
+       }
+       sortedMembers = firstLetter.join('').charAt(0).toUpperCase();
+       finalArray.push(sortedMembers);
+       firstLetter = [];
+      }
+    }
+    
+    return finalArray.sort().join('');
+  }
 }
 
 module.exports = {
