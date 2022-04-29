16db321acb9f10f7216087182d2b45b924b1a93a
DoroPomidoro <pomidorodoro@gmail.com>
2022-04-29 16:03:39 +0300

    fix: what season full solved

--- src/what-season.js
+++ src/what-season.js
@@ -11,37 +11,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(date) {
-  if(!date) {
-    return 'Unable to determine the time of year!'
-  } else {
-    if ((Object.prototype.toString.call(date) === '[object Date]') || (Object.getOwnPropertySymbols(date).length > 0 )) {
-      let month = date.getMonth();
-      if (month === 0 || month === 1 || month === 11) {
-        return "winter"
-      } else if (month === 2 || month === 3 || month === 4) {
-        return "spring" 
-      } else if (month === 5 || month === 6 || month === 7) {
-        return "summer"
-      } else if (month === 8 || month === 9 || month === 10) {
-        return "autumn"
-      } else {      
-      return 'Unable to determine the time of year!';
-      }
-    } else {
-      throw new Error("Invalid date!");
-    }
-  }
-}
-
-
 // function getSeason(date) {
 //   if(!date) {
 //     return 'Unable to determine the time of year!'
 //   } else {
-//     if (Object.prototype.toString.call(date) !== '[object Date]' || Object.getOwnPropertySymbols(date).length !== 0)  throw new Error("Invalid date!") 
-      
-    
+//     if ((Object.prototype.toString.call(date) === '[object Date]') || (Object.getOwnPropertySymbols(date).length !== 0 )) {
 //       let month = date.getMonth();
 //       if (month === 0 || month === 1 || month === 11) {
 //         return "winter"
@@ -54,8 +28,57 @@ function getSeason(date) {
 //       } else {      
 //       return 'Unable to determine the time of year!';
 //       }
-//     } 
+//     } else {
+//       throw new Error("Invalid date!");
+//     }
 //   }
+// }
+
+
+// function getSeason(date) {
+//   if(!date) {
+//     return 'Unable to determine the time of year!'; 
+//   } else {
+//     if ((Object.prototype.toString.call(date) === '[object Date]') || (Object.getOwnPropertySymbols(date).length !== 0 )) {
+//       debugger 
+//       console.log(date.getTime());
+//       let month = date.getMonth();
+//       if (month === 0 || month === 1 || month === 11) {
+//         return "winter";
+//       } else if (month === 2 || month === 3 || month === 4) {
+//         return "spring" 
+//       } else if (month === 5 || month === 6 || month === 7) {
+//         return "summer"
+//       } else if (month === 8 || month === 9 || month === 10) {
+//         return "autumn"
+//       } 
+//     } else {
+//       throw new Error("Invalid date!");
+//     }
+//   }
+// } 
+
+
+function getSeason(date) {
+  if(!date) {
+    return 'Unable to determine the time of year!'
+  } else {
+    if (Object.prototype.toString.call(date) !== '[object Date]' || Object.getOwnPropertySymbols(date).length !== 0)  throw new Error("Invalid date!") //test tricky and very tricky, 
+ //Object.getOwnPropertySymbols(date).length !== 0 need check above this string, it's very important! really!     
+      let month = date.getMonth();
+      if (month === 0 || month === 1 || month === 11) {
+        return "winter"
+      } else if (month === 2 || month === 3 || month === 4) {
+        return "spring" 
+      } else if (month === 5 || month === 6 || month === 7) {
+        return "summer"
+      } else if (month === 8 || month === 9 || month === 10) {
+        return "autumn"
+      } else {      
+      return 'Unable to determine the time of year!';
+      }
+    } 
+  }
 
 
 module.exports = {
